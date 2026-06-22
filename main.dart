6fd6785062ea90a5722228e2b4e14c43979135f1
Gutex <ggetj88@gmail.com>
2026-06-22 12:40:56 +0300

    Phase 2: Registration and login with validation

--- main.dart
+++ main.dart
@@ -1,8 +1,11 @@
 import 'dart:io';
 
+Map<String, Map<String, String>> users = {};
+
 void main(){
 
   bool isRunning = true;
+
   while (isRunning){
     print ('=== SMART JOB PORTAL');
     print ('1, Register');
@@ -14,10 +17,10 @@ void main(){
 
   switch (input){
     case '1':
-      print('Register chosen');
+      register();
       break;
     case '2':
-      print('Login chosen');
+      login();
       break;
     case '3':
       print('Goddbye!');
@@ -29,4 +32,100 @@ void main(){
 
   }
 
+}
+
+//........Register Function...........
+
+void register(){
+  print('\n--- Register ---');
+
+  //step 1: Get email
+  print('Email: ');
+  String? email = stdin.readLineSync();
+
+  //Check if email is empty
+  if (email == null || email.isEmpty){
+    print('Email cannot be empty');
+    return;
+  }
+
+  //Step 2: Check if email already exists
+  if (users.containsKey(email)){
+    print('Email already Registered!');
+    return;
+  }
+
+  //Step 3: Get password
+  print('Password: ');
+  String? password = stdin.readLineSync();
+
+  if (password == null || password.isEmpty){
+    print('Password cannot be empty!');
+    return;
+  }
+
+  //Step 4: Get name
+  print('Name: ');
+  String? name = stdin.readLineSync();
+
+  if (name == null || name.isEmpty){
+    print('Name cannot be empty!');
+    return;
+  }
+
+  //Step 5: Get role
+  print('Role (candidate/employer):');
+  String? role = stdin.readLineSync();
+
+  if (role == null || role.isEmpty){
+    print('Role cannot be empty');
+    return;
+  }
+
+  //Step 6: Add user to the map
+  users[email] = {
+    'password': password,
+    'name': name,
+    'role': role,
+  };
+  print('✅ Registration successful!');
+}
+
+//----------Login Function--------
+void login(){
+  print('\n--- Login ---');
+
+  //Step 1: Get email
+  String? email = stdin.readLineSync();
+
+  if(email == null || email.isEmpty){
+    print('Email cannot be empty!');
+    return;
+  }
+
+  //Step 2: check if email exists
+  if (!users.containsKey(email)){
+    print('Email not found! Please register first.');
+    return;
+  }
+
+  //Step 3: Get password
+  print('Password: ');
+  String? password = stdin.readLineSync();
+
+  if (password == null || password.isEmpty){
+    print('Password cannot be empty!');
+    return;
+  }
+
+  //Step 4: Check if password matches
+  if (users[email]!['password'] == password){
+    String name = users[email]!['name']!;
+    String role = users[email]!['role']!;
+
+    print('✅ Welcome back, $name!');
+    print('  Role: $role');
+  }else {
+    print('❌ Incorrect password!');
+  }
 }
