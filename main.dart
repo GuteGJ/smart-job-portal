e4811ed890a6f5974b80a7a0b79bff65729d7210
Gutex <ggetj88@gmail.com>
2026-06-22 11:33:30 +0300

    Phase 1: Working menu with user input and switch

--- main.dart
+++ main.dart
@@ -1,7 +1,32 @@
+import 'dart:io';
+
 void main(){
-  print ('=== SMART JOB PORTAL');
-  print ('1, Register');
-  print ('2, Login');
-  print ('3, Exit');
-  print ('Choose an option: ');
+
+  bool isRunning = true;
+  while (isRunning){
+    print ('=== SMART JOB PORTAL');
+    print ('1, Register');
+    print ('2, Login');
+    print ('3, Exit');
+    print ('Choose an option: ');
+  
+  String? input = stdin.readLineSync();
+
+  switch (input){
+    case '1':
+      print('Register chosen');
+      break;
+    case '2':
+      print('Login chosen');
+      break;
+    case '3':
+      print('Goddbye!');
+      isRunning =false;
+      break;
+    default:
+      print('Invalid choice. Please try again');
+  }
+
+  }
+
 }
